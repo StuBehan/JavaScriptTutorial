a09fa8c812ef3f3b5de09d9452366062ddb816d3
StuBehan <stuart.a.behan@gmail.com>
2021-01-25 21:06:31 +0000

    added if statements to index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-alert("Hello, World!")
+/*
+this is a comment
+*/
+
+// so is this 
+
+alert("Hello, World!");
+
+var areYouVegan = true;
+var myNum = 2;
+
+if(areYouVegan){
+    document.write("Here is the Vegan Menu");
+} else {
+    document.write("There are many exciting dishes on the menu I'm sure you'll enjoy");
+}
+
+if(myNum > 8){
+    document.write("The variable is greater than 8");
+} else {
+    document.write("The variable is not greater than 8");
+}
