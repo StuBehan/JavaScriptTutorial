5ddc1c92f848946781627720f3f5227af09fceda
StuBehan <stuart.a.behan@gmail.com>
2021-01-26 20:25:05 +0000

    added global variables exercise

--- functions.js
+++ functions.js
@@ -1,6 +1,18 @@
 function getAverage (a,b){
+    // local variable average
     var average = (a + b) / 2;
     console.log(average);
 }
 
-getAverage();
+getAverage(7,12);
+
+
+//global variable outside of function
+average = 0;
+
+function globalAverage (a,b){
+    average = (a + b) / 2;
+    console.log(average);
+}
+
+globalAverage(7,12);
